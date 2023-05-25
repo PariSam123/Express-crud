307d9719dc9b75ada6656dc6335eb5eaa4c2a1bb
Parija <parija.jamatia.pj@gmail.com>
2023-05-25 23:06:26 +0530

    Cart

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById(1)
+    User.findByPk(1)
       .then(user => {
         req.user = user;
         next();
@@ -34,13 +36,16 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
     .sync()
     .then(result => {
-        return User.findById(1);
-        // console.log(result);
-        app.listen(3000);
+        return User.findByPk(1);
+        // console.log(result);  
     })
     .then(user => {
         if (!user) {
@@ -50,8 +55,12 @@ sequelize
     })
       .then(user => {
         // console.log(user);
+        return user.createCart();
+    })
+      .then(cart => {
         app.listen(3000);
     })
       .catch(err => {
         console.log(err);
     });
+    
